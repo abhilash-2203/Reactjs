0b6c180522bb3f356a5876331407972fb0532c6a
abhilash-2203 <abhilashchandravanshi.ab123@gmail.com>
2024-02-15 23:38:22 +0530

    Tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import card from './Components/card'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    name:'abhilash',
+    age:21
+  }
+  let newArr=[1,2,3,4,5]
 
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4' >Tailwind test</h1>
-    <card/>
+    <Card username="chaiaurcode" btnText="click me" />
+    <Card username="Aur Bablu Bhaii" />
     </>
   )
 }
